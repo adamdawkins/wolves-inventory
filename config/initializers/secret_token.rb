4c6e7d4b523b3131674bb7b1e8dd2808584b14da
Adam Dawkins <adamdawkins@gmail.com>
2013-09-04 14:25:12 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inventory::Application.config.secret_key_base = '0c69754ed8b0aecf4ea524ffea096a10734b0927c34596ae44f5ad1abbea6e81c13ca6a4cace699c6ea51b5e0bbf257e9bcf6fb96ef3493f8a9c52b42bcf6334'
---
+++ test/features/add_inventory_items.feature
@@ -0,0 +1,14 @@
+Feature: Add inventory items to system
+  As the inventory manager
+  I want to add an inventory item to a the system,
+  so that I can track the team inventory.
+
+  * I should be able to add the following information:
+    - size
+    - quantity
+    - a label for unique identification
+    - purchase date
+  * I should be able to select an item type
+  * I should be able to tag an item with a squad.
+  * 'Contact Kit' items should not have a quantity - we want to track each one
+    individually, because they are expensive
