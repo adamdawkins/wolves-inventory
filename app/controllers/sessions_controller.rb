aae18a0137475ab542f37cf9591c6753fa6fce24
Adam Dawkins <adamdawkins@gmail.com>
2013-09-08 23:46:00 +0100

    Handle nil username in Sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    redirect_to root_path
+    if params[:username].nil?
+      flash.now[:error] = "We couldn't find a user with that username."    
+      render action: 'new'
+    else
+      redirect_to root_path
+    end
   end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe SessionsController do
+
+    describe "POST create" do 
+      describe "username parameter is nil" do
+        before { post :create, username: nil, password: 'password' }
+
+        it "renders the new template" do
+          assert_template :new
+        end
+
+        it "provides an error message" do 
+          (flash[:error]).must_equal "We couldn't find a user with that username."
+        end
+
+      end
+
+      describe "with correct credentials" do
+
+
+      end 
+    end
+
+  end
 end
